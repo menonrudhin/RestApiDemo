f1e48f14e348dce30e1fcd2a6ee90458ecdd1404
Rudhin Menon <menonrudhin@gmail.com>
2025-08-05 21:21:37 -0400

    implementing WebFlux step by step

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,1 +1,5 @@
-insert into BOOK (id, name, author, published) values (1,'Harry Potter','J.K.Rowling',19930404);
+INSERT INTO BOOK (id, name, author, published) VALUES (1, 'Harry Potter', 'J.K.Rowling', 19930404);
+INSERT INTO BOOK (id, name, author, published) VALUES (2, 'The Hobbit', 'J.R.R. Tolkien', 19370921);
+INSERT INTO BOOK (id, name, author, published) VALUES (3, '1984', 'George Orwell', 19490608);
+INSERT INTO BOOK (id, name, author, published) VALUES (4, 'To Kill a Mockingbird', 'Harper Lee', 19600711);
+INSERT INTO BOOK (id, name, author, published) VALUES (5, 'The Da Vinci Code', 'Dan Brown', 20030318);
